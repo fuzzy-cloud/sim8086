1f64fcc32a604d9b7288ee71b8e83dbfe533273e
Mikhail Nevzorov <mikhail.zronev@gmail.com>
2025-05-17 10:19:25 +0300

    decode simple MOV instruction

--- cpu.go
+++ cpu.go
@@ -1,1 +1,123 @@
 package cpu
+
+import "fmt"
+
+type opcode byte
+
+const (
+	opcodeInvalid opcode = iota
+	MOV
+)
+
+type register byte
+
+const (
+	registerInvalid register = iota
+	AL
+	CL
+	DL
+	BL
+	AH
+	CH
+	DH
+	BH
+	AX
+	CX
+	DX
+	BX
+	SP
+	BP
+	SI
+	DI
+)
+
+var opcodeToString = map[opcode]string{
+	opcodeInvalid: "INVALID",
+	MOV:           "mov",
+}
+
+var registerToString = map[register]string{
+	registerInvalid: "INVALID",
+	AL:              "al",
+	CL:              "cl",
+	DL:              "dl",
+	BL:              "bl",
+	AH:              "ah",
+	CH:              "ch",
+	DH:              "dh",
+	BH:              "bh",
+	AX:              "ax",
+	CX:              "cx",
+	DX:              "dx",
+	BX:              "bx",
+	SP:              "sp",
+	BP:              "bp",
+	SI:              "si",
+	DI:              "di",
+}
+
+var mapModeAndWidthToRegister = map[byte][2]register{
+	0b000: {AL, AX},
+	0b001: {CL, CX},
+	0b010: {DL, DX},
+	0b011: {BL, BX},
+	0b100: {AH, SP},
+	0b101: {CH, BP},
+	0b110: {DH, SI},
+	0b111: {BH, DI},
+}
+
+type decodeResult struct {
+	op  opcode
+	d   bool
+	w   int
+	mod byte
+	reg byte
+	rm  byte
+}
+
+func decode(stream []byte) decodeResult {
+	var res decodeResult
+
+	b1 := stream[0]
+	b2 := stream[1]
+
+	switch {
+	case b1>>2 == 0b100010:
+		// b1
+		res.op = MOV
+		res.d = b1>>1&0x1 == 1
+		res.w = int(b1 & 0x1)
+
+		// b2
+		res.mod = b2 >> 6
+		res.reg = b2 >> 3 & 0b111
+		res.rm = b2 & 0b111
+	default:
+		panic("unsupported instruction")
+	}
+
+	return res
+}
+
+func disassemble(stream []byte) (string, error) {
+	if len(stream) < 2 {
+		return "", fmt.Errorf("byte stream should at least have 2 bytes, got — %d", len(stream))
+	}
+
+	res := decode(stream)
+
+	switch res.mod {
+	case 0b11:
+		operand1 := mapModeAndWidthToRegister[res.reg][res.w]
+		operand2 := mapModeAndWidthToRegister[res.rm][res.w]
+
+		opStr := opcodeToString[res.op]
+		dstStr := registerToString[operand2]
+		srcStr := registerToString[operand1]
+
+		return fmt.Sprintf("bits 16\n\n%s %s, %s", opStr, dstStr, srcStr), nil
+	default:
+		return "", fmt.Errorf("unsupported MOD value: %b", res.mod)
+	}
+}
--- cpu_test.go
+++ cpu_test.go
@@ -2,13 +2,35 @@ package cpu
 
 import (
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestDecode(t *testing.T) {
-	bs, err := os.ReadFile("listings/0037_single_register_mov")
+	stream, err := os.ReadFile("listings/0037_single_register_mov")
 	require.NoError(t, err)
-	require.Len(t, bs, 2)
+
+	asmFile, err := os.ReadFile("listings/0037_single_register_mov.asm")
+	require.NoError(t, err)
+
+	insideComment := false
+	want := strings.TrimLeftFunc(string(asmFile), func(r rune) bool {
+		if r == ';' {
+			insideComment = true
+		}
+		if r == '\n' {
+			insideComment = false
+			return true
+		}
+		return insideComment
+	})
+	want = strings.TrimSpace(want)
+
+	t.Logf("\n%b\n", stream)
+
+	got, err := disassemble(stream)
+	require.NoError(t, err)
+	require.Equal(t, want, got)
 }
